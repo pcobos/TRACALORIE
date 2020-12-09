7f1922bbf6b793e817b2be70cad5a630ca090261
Pedro Cobos <pedro.cobos@live.com>
2020-12-08 23:29:24 -0600

    added function controllers

---
+++ app.js
@@ -0,0 +1,23 @@
+// Storage Controller
+// const StrCtrl = (function (){
+
+// })();
+
+// Item Controller
+const ItemCtrl = (function(){
+  // Inside, we should do the item constructor
+  // Data structure / State
+})();
+
+// UI Controller
+const UICtrl =  (function(){
+
+})();
+
+// App Controller
+const App = (function(ItemCtrl, UICtrl, ){
+
+})(ItemCtrl, UICtrl);
+
+// First thing to initialize the controllers is to use a IIFE Function (Immediately Invoked Function Expressions)
+
