8957b18966b684a794e40c0266833cf4bbfa9eb6
Pedro Cobos <pedro.cobos@live.com>
2020-12-24 17:51:45 -0600

    refactor: add separate files for Item Controller and UI Controller

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-import { ItemCtrl } from './item-controller';
 
 // Storage Controller
 // const StrCtrl = (function (){
@@ -6,77 +5,77 @@ import { ItemCtrl } from './item-controller';
 // })();
 
 // Item Controller
-// const ItemCtrl = (function(){
-//   // Inside, we should do the item constructor
-//   const Item = function(id, name, calories){
-//     this.id = id;
-//     this.name = name;
-//     this.calories = calories;
-//   }
-//   // Data structure / State
-//   const data = {
-//     items: [
-//       // {id: 0, name:"Hamburger", calories:1200},
-//       // {id: 1, name:"French Fries", calories:800},
-//       // {id: 2, name:"Soda", calories:950}
-//     ],
-//     currentItem: null,
-//     totalCalories: 0
-//   }
+const ItemCtrl = (function(){
+  // Inside, we should do the item constructor
+  const Item = function(id, name, calories){
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  }
+  // Data structure / State
+  const data = {
+    items: [
+      // {id: 0, name:"Hamburger", calories:1200},
+      // {id: 1, name:"French Fries", calories:800},
+      // {id: 2, name:"Soda", calories:950}
+    ],
+    currentItem: null,
+    totalCalories: 0
+  }
 
-//   // Since the previous data is private, we need to return it in order to access it for testing purposes
+  // Since the previous data is private, we need to return it in order to access it for testing purposes
 
-//   // Public methods
-//   return {
-//     // Following function is for getting the items and passing them to our App controller
-//     getItems: function(){
-//       return data.items;
-//     },
+  // Public methods
+  return {
+    // Following function is for getting the items and passing them to our App controller
+    getItems: function(){
+      return data.items;
+    },
 
-//     addItem: function(name, calories){
-//       // Add logic for ID
-//       let ID;
-//       if (data.items.length > 0){
-//         ID = data.items[data.items.length - 1].id + 1;
-//       } else {
-//         ID = 0;
-//       }
+    addItem: function(name, calories){
+      // Add logic for ID
+      let ID;
+      if (data.items.length > 0){
+        ID = data.items[data.items.length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
       
-//       // Convert calories to integer
-//       calories = parseInt(calories);
+      // Convert calories to integer
+      calories = parseInt(calories);
       
-//       // Create new item with info from inputs
-//       newMeal = new Item(ID, name, calories); 
+      // Create new item with info from inputs
+      newMeal = new Item(ID, name, calories); 
       
-//       // Push item to data structure.
-//       data.items.push(newMeal);
+      // Push item to data structure.
+      data.items.push(newMeal);
 
-//       // Returning the new meal for later use in variables
-//       return newMeal;
-//     },
+      // Returning the new meal for later use in variables
+      return newMeal;
+    },
 
-//     // Get total calories function
-//     getTotalCalories: () => {
-//       let sum = 0;
+    // Get total calories function
+    getTotalCalories: () => {
+      let sum = 0;
 
-//       // Loop through items and add cals
-//       data.items.forEach(function(item){
-//         sum += item.calories;
-//       })
+      // Loop through items and add cals
+      data.items.forEach(function(item){
+        sum += item.calories;
+      })
 
-//       // Set total cal in data structure
-//       data.totalCalories = sum;
+      // Set total cal in data structure
+      data.totalCalories = sum;
 
-//       // Return total
-//       return data.totalCalories;
-//     },
+      // Return total
+      return data.totalCalories;
+    },
 
-//     // Method to check inner workings of data structure
-//     logData: function(){
-//       return data;
-//     },
-//   }
-// })();
+    // Method to check inner workings of data structure
+    logData: function(){
+      return data;
+    },
+  }
+})();
 
 // UI Controller
 const UICtrl =  (function(){
@@ -156,6 +155,7 @@ const UICtrl =  (function(){
 
 // App Controller
 const App = (function(ItemCtrl, UICtrl){
+
     // Load event listeners
   const loadEventListeners = function(){
     
